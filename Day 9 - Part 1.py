fce2233c8aa9019fee86554d455d582f93a13c29
Gramineae <2843861030@qq.com>
2020-12-09 19:10:04 +0800

    Add files via upload

---
+++ Day 9 - Part 1.py
@@ -0,0 +1,21 @@
+fhand = open('day9.txt')
+XMAS = [int(n) for n in fhand]
+
+def check_validity(total, numbers):
+    """check whether the sum is valid."""
+    for n in numbers:
+        complement = total - n
+        if (complement != n) and (complement in numbers):
+            return True
+    return False
+    
+
+for i,number in enumerate(XMAS):
+    if i < 25: 
+        continue
+    previous25 = XMAS[(i-25):i]
+    if not check_validity(number, previous25):
+        break
+
+print(number)
+        
